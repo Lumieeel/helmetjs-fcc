d17ba9f8e5513b62a4b32a1300880d814c4de753
Lumieeel <bemjamintorooyarzun@gmail.com>
2025-09-14 00:00:22 -0300

    FCC Information Security with HelmetJS - reto 2

--- myApp.js
+++ myApp.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+/****** Solution code starts here ******/
 
+app.use(helmet.hidePoweredBy());
 
+/****** Solution code ends here ******/
 
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
 
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
 
-
-
-
-
-
+module.exports = app;
 
 
 

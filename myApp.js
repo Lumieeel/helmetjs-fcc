5a134997e2207370598c1c551f49c5a446216818
Lumieeel <bemjamintorooyarzun@gmail.com>
2025-09-15 13:19:21 -0300

    Reto 3 - resolver conflicto y aplicar frameguard

--- myApp.js
+++ myApp.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 const app = express();
 
-/****** Solution code starts here ******/
+const api = require("./server.js");
 
-app.use(helmet.frameguard({ action: 'deny' }));
+app.use(express.static("public"));
 
-/****** Solution code ends here ******/
+// Reto 2
+app.use(helmet.hidePoweredBy());
 
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+// Reto 3
+app.use(helmet.frameguard({ action: "deny" }));
+
+app.disable("strict-transport-security");
+app.use("/_api", api);
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 let port = process.env.PORT || 3000;
@@ -23,44 +25,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});

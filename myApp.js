4dbd5da486886b1b0b2da972a10cc8c6ebf9c518
Lumieeel <bemjamintorooyarzun@gmail.com>
2025-09-16 13:41:45 -0300

    Reto 8 - hsts

--- myApp.js
+++ myApp.js
@@ -22,7 +22,16 @@ app.use(helmet.hidePoweredBy());
 // app.use(helmet.dnsPrefetchControl({ allow: false }));
 
 // Reto 7
-app.use(helmet.ieNoOpen());
+// app.use(helmet.ieNoOpen());
+
+// Reto 8
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(
+  helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: true,
+  })
+);
 
 app.use(express.static("public"));
 app.disable("strict-transport-security");
@@ -36,3 +45,4 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
